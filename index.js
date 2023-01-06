d16b7ed4ffc75aaf30c83dd7336f59f875a06403
Venkatakrishnan R <krishnanvenkat4@gmail.com>
2023-01-07 00:11:59 +0530

    changes aded

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const app = express();
 require("dotenv").config();
 
 app.get("/", (req, res) => {
-  res.send(
-    `<html>
-    <head>
-      <title>Server</title>
-    </head>
-    <body  style="background-color: 410093; color: white; min-height : vh ; font-size: 2rem; font-family : Sans-serif ;" >
-     <div style="display: flex ; justify-content: center; align-items: center;">
-        <h1>${process.env.NODE_ENV} Server is up and running</h1>
-     </div>
-    </body>
-  </html>`
-  );
+  res.send(`<html>
+  <head>
+    <title>Server</title>
+    <style>
+    *{
+      padding-top : 4rem;
+      margin : 0;
+    }
+      body {
+        background: #410093;
+        color: white;
+      }
+      h1 {
+        font-size : 5rem;
+        margin: 0;
+        padding:0;
+        text-align: center;
+        font-family: 'arial';
+      }
+    </style>
+  </head>
+  <body>
+      <h1>server is up and running</h1>
+  </body>
+</html>`);
 });
 
 app.listen(process.env.PORT, () => {
